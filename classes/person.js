d7e7de79d7f8441585362f29736d91e6d042f0bd
unknown <abarghouei.mahsa@gmail.com>
2023-10-09 19:14:25 +0330

    completed class_syntax

--- classes/person.js
+++ classes/person.js
@@ -1,5 +1,37 @@
 // Your code here
 
+class Person {
+  constructor(firstName, lastName, age){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age
+  }
+
+ // Example of an instance method
+  introduce(){
+    console.log(`Hi, I'm ${this.firstName} ${this.lastName}, and I'm ${this.age} years old.`);
+  }
+
+  static introducePeople(persons){
+
+   if(!Array.isArray(persons)){
+    throw new Error("introducePeople only takes an array as an argument.");
+   }
+
+   for(const person of persons){
+    if(!(person instanceof Person)){
+      throw new Error("All items in array must be Person class instances.");
+    }
+   }
+
+   for(const person of persons){
+    person.introduce();
+   }
+  }
+}
+
+
+
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
